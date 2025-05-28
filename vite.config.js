53c9db9ac97ee1531cfaf00edb4e867a30e9f5c0
Sander Wiegers <wiegers@smart2it.nl>
2025-05-28 13:30:36 +0200

    fix issues

--- vite.config.js
+++ vite.config.js
@@ -4,31 +4,31 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), visualizer({
-    filename: './dist/stats.html',
-    template: 'treemap', // optional: 'sunburst' or 'treemap'
-    gzipSize: true,
-    brotliSize: true,
-  })],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    lib: {
-      entry: 'src/index.js',
-      name: 'react-virtualized-tree',
-      formats: ['es', 'cjs', 'umd'],
-      fileName: (format) => `react-virtualized-tree.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-virtualized'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          'react-virtualized': 'ReactVirtualized',
+    plugins: [react(), visualizer({
+        filename: './dist/stats.html',
+        template: 'treemap', // optional: 'sunburst' or 'treemap'
+        gzipSize: true,
+        brotliSize: true,
+    })],
+    build: {
+        outDir: 'dist',
+        sourcemap: true,
+        lib: {
+            entry: 'src/index.js',
+            name: 'react-virtualized-tree',
+            formats: ['es', 'cjs', 'umd'],
+            fileName: (format) => `react-virtualized-tree.${format}.js`
         },
-      },
-    },
-    minify: false,
-  }
+        rollupOptions: {
+            external: ['react', 'react-dom', 'react-virtualized'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                    'react-virtualized': 'ReactVirtualized',
+                },
+            },
+        },
+        minify: false,
+    }
 })
